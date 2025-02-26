79acc9622ec899d3c07582a1012056ed6bc58e5e
ManamiTakahara <uni202412_09@telework.to>
2025-02-26 11:29:15 +0900

    Initial commit

---
+++ SoapBubblesClick/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SoapBubblesClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scorLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // scorLabel
+            // 
+            this.scorLabel.AutoSize = true;
+            this.scorLabel.Location = new System.Drawing.Point(69, 24);
+            this.scorLabel.Name = "scorLabel";
+            this.scorLabel.Size = new System.Drawing.Size(15, 15);
+            this.scorLabel.TabIndex = 0;
+            this.scorLabel.Text = "0";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Location = new System.Drawing.Point(565, 24);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(23, 15);
+            this.timerLabel.TabIndex = 1;
+            this.timerLabel.Text = "30";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "得点：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(505, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "残り時間：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.scorLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scorLabel;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SoapBubblesClick/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoapBubblesClick
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //変数の宣言
+            //Bubbleインスタンスの生成
+            Bubble bubble = new Bubble();
+            //Bubbleの親コンテナはフォームに設定
+            bubble.Parent = this;
+
+            BackGround backGround = new BackGround();
+            int score;
+            //
+            int rimainTime;
+
+            //概要のコメント
+            //シャボン玉の動き
+            //得点が加算される
+            //時間が減る
+            //ゲームオーバーの判定
+
+        }
+    }
+}
